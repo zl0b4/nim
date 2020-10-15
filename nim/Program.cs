ffc5ec8b294b3f349bdb2b368c94f726108745f1
zl0b4 <evgeniypenkov2015@gmail.com>
2020-10-15 17:20:27 +0500

    Добавьте файлы проекта.

---
+++ nim/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace nim
+{
+    class Program
+    {
+        static void PrintArray(string [][] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr[i].Length; j++)
+                    Console.Write(arr[i][j]);
+                Console.WriteLine();
+            }
+        }
+        static int Turn(ref string [][] elems, string player, ref int counter)
+        {
+            Console.WriteLine($"Ходит {player} игрок!");
+            Console.Write("Введите номер строки и количество спичек, которые хотите убрать справа, через пробел: ");
+            int[] choice = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int raw = choice[0];
+            int count = choice[1];
+            elems[raw - 1] = new string[elems[raw - 1].Length - choice[1]];
+            for (int i = 0; i < elems[raw - 1].Length; i++)
+                elems[raw - 1][i] = "|";
+            PrintArray(elems);
+            if (elems[raw - 1].Length == 0)
+                counter++;
+            return counter;
+        }
+        static void Main(string[] args)
+        {
+            int emptyRawsCounter = 0;
+            int playerCheck = 2;
+            string[] players = { "первый", "второй" };
+            string[][] elems = new string[4][];
+            for (int i = 0, j = 1; i < elems.Length && j <= 7; i++, j+=2)
+                elems[i] = new string[j];
+
+            for (int i = 0; i < elems.Length; i++)
+            {
+                for (int j = 0; j < elems[i].Length; j++)
+                    elems[i][j] = "|";
+            }
+            
+            string playerNumber = string.Empty;
+            Console.WriteLine(@"Ним — игра, в которой два игрока по очереди берут предметы, разложенные на несколько кучек.
+За один ход может быть взято любое количество предметов (большее нуля) из одной кучки.
+ПРОИГРЫВАЕТ игрок, взявший последний предмет.
+Количество кучек = 4, в каждой кучке 1, 3, 5, 7.
+
+Вводите номер кучки и количество предметов через пробел.
+Например: 2 4
+
+Нажмите любую клавишу для начала игры.");
+            Console.ReadKey();
+            while (emptyRawsCounter != 4)
+            { 
+                Console.Clear();
+                PrintArray(elems);
+                playerNumber = playerCheck % 2 == 0 ? players[0] : players[1];
+                try
+                {
+                    emptyRawsCounter = Turn(ref elems, playerNumber, ref emptyRawsCounter);
+                    playerCheck++;
+                }
+                catch (Exception)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Ошибка!\n");
+                    Console.WriteLine("Нажмите любую клавишу для продолжения игры.");
+                    Console.ReadKey();
+
+                }
+                
+            }
+            Console.Clear();
+            string winner = playerNumber == players[0] ? players[1] : players[0];
+            Console.WriteLine($"Победил {winner} игрок!");
+        }
+    }
+}
---
+++ nim/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("nim")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("nim")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("61d175aa-1c3f-42c2-8a6b-5f12a8819f0d")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
